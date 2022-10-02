b0df7379cd7dc0cf4d745282e301373104d16d7b
Vitalik <05vp94@mail.ru>
2022-10-02 19:34:50 +0300

    changes before lesson 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Tehnologies from "./Tehnologies";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header/>
+        <Tehnologies/>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+    return(
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
+export default Header
